1c554a1612d04d8a9c2c446ef87c325f21949c2d
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 09:56:17 +0700

    menambahkan operasi pemanggilan dari dao getMhs di RepositoryMhs

--- app/src/main/java/com/example/pam7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pam7/repository/RepositoryMhs.kt
@@ -8,5 +8,9 @@ interface RepositoryMhs {
 //    Untuk menyimpan data yang sudah di input
 //    operasi sesuai dengan ada yang di DAO
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+//    Operasi memanggil dari MahasiswaDao untuk dapat semua aliran data (Flow)
     fun getALlMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
