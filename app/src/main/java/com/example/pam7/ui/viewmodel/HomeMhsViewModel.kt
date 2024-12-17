23b61c7d63ffdbd987055b92a1b5b315334aa634
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 12:05:52 +0700

    menambahkan .stateIn

--- app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.pam7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pam7.data.entity.Mahasiswa
 import com.example.pam7.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (private val repositoryMhs: RepositoryMhs): ViewModel() {
     val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
@@ -32,6 +35,13 @@ class HomeMhsViewModel (private val repositoryMhs: RepositoryMhs): ViewModel() {
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
 }
 
 data class HomeUiState(
