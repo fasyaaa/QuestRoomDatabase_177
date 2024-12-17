527937b2dd076d4fb79ec550ebb1759dad043911
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 12:25:48 +0700

    menambahkan data class DetailUiState

--- app/src/main/java/com/example/pam7/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/DetailMhsViewModel.kt
@@ -4,10 +4,21 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pam7.repository.RepositoryMhs
 import com.example.pam7.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ): ViewModel() {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
 }
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isErrorState: Boolean = false,
+    val errorMessage: String = ""
+)
