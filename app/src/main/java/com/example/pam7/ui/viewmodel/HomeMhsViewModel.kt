cf90fdb9b251fff180e61f629729bd7445621b6a
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 12:07:04 +0700

    menambahkan data class HomeUiState bagian isError dan errorMessage

--- app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
@@ -47,4 +47,6 @@ class HomeMhsViewModel (private val repositoryMhs: RepositoryMhs): ViewModel() {
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
     val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
 )
