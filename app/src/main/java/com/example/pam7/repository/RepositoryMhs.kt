14346e0bdce1995d56435f748b76b70ac90cc898
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 09:54:48 +0700

    menambahkan operasi pemanggilan dari dao getAllMhs di RepositoryMhs

--- app/src/main/java/com/example/pam7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pam7/repository/RepositoryMhs.kt
@@ -1,10 +1,12 @@
 package com.example.pam7.repository
 
 import com.example.pam7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 // berhubungan dengan view model agar bisa di akses di view model
 interface RepositoryMhs {
 //    Untuk menyimpan data yang sudah di input
 //    operasi sesuai dengan ada yang di DAO
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getALlMhs(): Flow<List<Mahasiswa>>
 }
