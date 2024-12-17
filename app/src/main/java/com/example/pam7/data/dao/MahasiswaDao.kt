63e5eea645835b654f63dc74555a4adc18f26733
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 09:53:06 +0700

    menambahkan operasi updateMahasiswa di dao

--- app/src/main/java/com/example/pam7/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pam7/data/dao/MahasiswaDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.pam7.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -23,5 +24,8 @@ interface MahasiswaDao {
 
     @Delete
     suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
 
