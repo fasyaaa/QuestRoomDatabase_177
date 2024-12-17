47dc3330191bc7bcbf8a3478d442298a9cd5d953
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 12:02:42 +0700

    menambahkan .onStart

--- app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,19 +1,30 @@
 package com.example.pam7.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.pam7.data.entity.Mahasiswa
 import com.example.pam7.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class HomeMhsViewModel (private val repositoryMhs: RepositoryMhs): ViewModel() {
     val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
         .filterNotNull()
         .map {
-
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
         }
 }
 
 data class HomeUiState(
-
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
 )
