10c5174d41ea0aeade3f4bd5bef285c0913017f8
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 14:23:28 +0700

    menambahkan bagian HomeMhsViewModel.kt

--- app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,12 +1,23 @@
 package com.example.pam7.ui.viewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam7.data.entity.Mahasiswa
 import com.example.pam7.repository.RepositoryMhs
+import com.example.pam7.ui.costumwidget.TopAppBar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -59,6 +70,50 @@ fun HomeMhsViewModel(
     viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
     onAddMhs: () -> Unit,
     onDetailClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+
+
+            }
+        }
+    ){
+        innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
 
