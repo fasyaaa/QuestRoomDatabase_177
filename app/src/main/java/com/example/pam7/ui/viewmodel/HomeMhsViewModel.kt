3db64fb3707e7fd62cd85921eb2a076014b17538
fasyaaa <muhammad.a.f.2210@gmail.com>
2024-12-17 18:56:20 +0700

    menghapus bagian yang salah

--- app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,17 +1,30 @@
 package com.example.pam7.ui.viewmodel
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,6 +39,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class HomeMhsViewModel (private val repositoryMhs: RepositoryMhs): ViewModel() {
     val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
@@ -64,57 +78,3 @@ data class HomeUiState(
     val isError: Boolean = false,
     val errorMessage: String = ""
 )
-
-@Composable
-fun HomeMhsViewModel(
-    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
-    onAddMhs: () -> Unit,
-    onDetailClick: (String) -> Unit,
-    modifier: Modifier = Modifier
-){
-    Scaffold (
-
-        topBar = {
-            TopAppBar(
-                judul = "Daftar Mahasiswa",
-                showBackButton = false,
-                onBack = { },
-                modifier = modifier,
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = onAddMhs,
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Tambah Mahasiswa",
-                )
-
-
-            }
-        }
-    ){
-        innerPadding ->
-        val homeUiState by viewModel.homeUiState.collectAsState()
-
-        BodyHomeMhsView(
-            homeUiState = homeUiState,
-            onClick = {
-                onDetailClick(it)
-            },
-            modifier = Modifier.padding(innerPadding)
-        )
-    }
-}
-
-@Composable
-fun BodyHomeMhsView(
-    homeUiState: HomeUiState,
-    onClick: (String) -> Unit,
-    modifier: Modifier = Modifier
-){
-
-}
